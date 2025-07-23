990a9457a316fd28d475e06dc099eb9374befd25
KiBohr <katharinabohr@web.de>
2025-07-23 13:12:28 +0200

    debugged, img + font path changed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,12 +4,28 @@ import Socials from "../socials/Socials";
 const Footer = () => {
     return ( 
         <footer className="bg-butter px-5 py-6 flex items-center justify-between lg:h-25 mt-auto">
-           <Logo text=".Vecipes" img="/src/assets/img/Tomate_b.png" imgH="h-8 lg:h-11" imgW="w-8 lg:w-11"/>
+           <Logo text=".Vecipes" img="/public/img/Tomate_b.png" imgH="h-8 lg:h-11" imgW="w-8 lg:w-11"/>
             <div className="flex gap-3 items-center justify-end">
-               <Socials imgPath="/src/assets/img/I_brown.png" desc="logo of instagram" to="/notBaked"/>
-               <Socials imgPath="/src/assets/img/T_brown.png" desc="logo of tictoc" to="/notBaked"/>
-               <Socials imgPath="/src/assets/img/E_brown.png" desc="logo of an at" to="/notBaked"/>
-               <Socials imgPath="/src/assets/img/L_brown.png" desc="logo of LinkedIn" to="https://www.linkedin.com/in/kiwi-b-3380bb352/"/>
+
+               <Socials 
+               imgPath="/public/img/I_brown.png" 
+               desc="logo of instagram" 
+               to="/notBaked"/>
+
+               <Socials 
+               imgPath="/public/img/T_brown.png" 
+               desc="logo of tictoc" 
+               to="/notBaked"/>
+
+               <Socials 
+               imgPath="/public/img/E_brown.png" 
+               desc="logo of an at" 
+               to="/notBaked"/>
+
+               <Socials 
+               imgPath="/public/img/L_brown.png" 
+               desc="logo of LinkedIn" 
+               to="https://www.linkedin.com/in/kiwi-b-3380bb352/"/>
                
             </div>
         </footer>
