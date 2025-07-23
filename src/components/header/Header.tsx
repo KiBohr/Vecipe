638e07a5e61fec872544ca0f0074bb92995ee3dd
KiBohr <katharinabohr@web.de>
2025-07-23 13:17:42 +0200

    debugging header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return ( 
         <header className="bg-butter flex flex-col h-[12vh] justify-center lg:h-25">
             <div className="flex items-center justify-between px-5 py-7">
-                <Logo img="/public/img/Tomate_b.png" imgH="h-9 lg:h-12" imgW="w-9 lg:w-12" />
+                <Logo img="/img/Tomate_b.png" imgH="h-9 lg:h-12" imgW="w-9 lg:w-12" />
                 <Nav/>
             </div>
             
